ec748d4aa91411ef44d4c9c0d9e8bc1942dc5833
Rafael Angeline <me@rafaelangeline.com>
2020-04-16 19:30:36 -0300

    Add dns records response specs

--- spec/get_dns_records_spec.rb
+++ spec/get_dns_records_spec.rb
@@ -16,4 +16,56 @@ describe "[GET] dns_records route", :type => :request do
     expect(response).to have_http_status(:bad_request)
   end
 
+  it 'should returns all dns records when only page param is passed' do
+    params = {
+      :page => 1,
+    }
+
+    get '/api/v1/dns_records', :params => params
+
+    body = JSON.parse(response.body)
+
+    expect(body["total_records"]).to eq(5)
+    expect(body["related_hostnames"].size).to eq(5)
+    expect(body["records"].size).to eq(5)
+  end
+
+  it 'should filter based on included/excluded params' do
+    params = {
+      :page => 1,
+      :included => "ipsum.com,dolor.com",
+      :excluded => "sit.com",
+    }
+
+    get '/api/v1/dns_records', :params => params
+
+    body = JSON.parse(response.body)
+
+    expect(body["total_records"]).to eq(2)
+    expect(body["related_hostnames"].size).to eq(2)
+    expect(body["records"].size).to eq(2)
+
+    expect(body["records"]).to eq([
+      {
+        "ip_address" => "1.1.1.1",
+        "id" => 1,
+      },
+      {
+        "ip_address" => "3.3.3.3",
+        "id" => 3,
+      },
+    ])
+
+    expect(body["related_hostnames"]).to eq([
+      {
+        "hostname" => "lorem.com",
+        "count" => 1,
+      },
+      {
+        "hostname" => "amet.com",
+        "count" => 2,
+      },
+    ])
+  end
+
 end

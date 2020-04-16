1ce86feef4ec65255a5c794e3beb189cd1a2e195
Rafael Angeline <me@rafaelangeline.com>
2020-04-16 19:21:21 -0300

    Create dns record and add hostnames relationship

--- app/controllers/api/v1/dns_records_controller.rb
+++ app/controllers/api/v1/dns_records_controller.rb
@@ -1,7 +1,33 @@
 class Api::V1::DnsRecordsController < ActionController::API
 
   def create
+      # Filter params.
+      dns_record_params = params.require(:dns_records).permit(:ip, hostnames_attributes: [:hostname])
+
+      # Create DNS record.
+      dns_record = DnsRecord.new(dns_record_params[:ip])
+
+      # Find or Create the hostnames.
+      hostnames = []
+
+      dns_record_params[:hostnames_attributes].each do |h|
+          hostnames << Hostname.find_or_create_by( { hostname: h[:hostname] } )
+      end
+
+      # Assign the hostnames to the DNS record.
+      dns_record.hostnames = hostnames
+
+      # Save.
+      dns_record.save
+
+      # Check if persisted.
+      if ! dns_record.persisted?
+          return render json => {
+              text: "Failed to save DNS record."
+          }, status: :bad_gateway
+      end
+
       # Return new record ID.
-      render json: {}, status: :ok
+      render json: { id: dns_record.id }, status: :ok
   end
 end
